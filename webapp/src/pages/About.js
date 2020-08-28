5cf9e4a9ca4d724da2ce8fb8d008fa4808fd03b2
Brice Rising <brice@algorand.com>
2020-08-27 22:56:27 -0400

    Fix ignite image
    The one that was here before seems to have gone down

--- webapp/src/pages/About.js
+++ webapp/src/pages/About.js
@@ -7,7 +7,7 @@ import {
 export default function About(props) {
   return (
     <Container className="d-flex flex-column align-items-center">
-      <img className="w-25" src="https://thumbs.dreamstime.com/b/fire-flame-icon-isolated-bonfire-sign-emoticon-symbol-white-emoji-logo-illustration-vector-142833014.jpg" />
+      <img className="w-25" src="ignite.jpg" />
       <h1>Ignite</h1>
       <p>We are a crowdfunding platform built on block chain. We use Algorand smart contracts to create conditional, temporary accounts that either pay out when the funding goal is met, or return the funds to investors if the time limit runs out. This way, investors will be able to give new projects just the spark they need.. to ignite!</p>
     </Container>
